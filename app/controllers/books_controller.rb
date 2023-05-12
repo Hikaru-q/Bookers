dbd6ffde0b2618248875d017519fbd216a88f60a
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-05-12 06:48:33 +0000

    Bookers 5/12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book)  
+      redirect_to @book 
     else
       @books = Book.all
       render :index
@@ -22,17 +22,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book.save
-      redirect_to edit_book_path(@book.id)
-    else
-      render :show
-    end
+    
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(@book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end  
   end
   
   def destroy
